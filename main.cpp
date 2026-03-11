547fed8f318fe96b1020f87f61be127b52747a8d
Rv <68052656+harvey-kun@users.noreply.github.com>
2026-03-11 09:16:36 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,31 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <cstdlib> ")
 
 using namespace std;
 
 void header(string txt);
 int choices(string txt1, string txt2);
-
+int firstscreen(string txt);
 
 int main(){
-    int option = 0;
+    int option = 0, stu_max = 0, quiz_max = 0;
 
     header("GRADE ENCODING");
     option = choices("Start Recording", "Exit");
+
+    if(option == 1){
+        system("clear");
+        header("GETTING INFO");
+        stu_max = firstscreen("How many Students should be recorded? :");
+        quiz_max = firstscreen("What's the total score of the activity? :");
+    }else{
+        return 0;
+    }
+
+    cout << "Student "<< stu_max << endl;
+    cout << "Quiz "<< quiz_max << endl;
 
     return 0;
 }
@@ -35,11 +48,20 @@ int choices(string txt1, string txt2){
         cin >> picked;
 
         if (picked != 1 && picked != 2) {
-            system("CLS");
+            system("clear");
             header("GRADE ENCODING");
         }
 
     } while (picked != 1 && picked != 2);
 
     return picked;
+}
+
+int firstscreen(string txt){
+    int maxcount = 0;
+
+    cout << txt;
+    cin >> maxcount;
+
+    return maxcount;
 }
