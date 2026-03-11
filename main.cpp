551221a976486f22c9a500a39f3be1c8c6c00b4f
Rv <68052656+harvey-kun@users.noreply.github.com>
2026-03-11 08:39:51 +0800

    Refactor choices function for input validation

--- main.cpp
+++ main.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 #include <iomanip>
+#include <string>
+
 using namespace std;
 
 void header(string txt);
-int choices(string txt1,string txt2);
+int choices(string txt1, string txt2);
+
 
 int main(){
     int option = 0;
-    header("GRADING");
-    option = choices("Start Recording","Exit");
 
+    header("GRADE ENCODING");
+    option = choices("Start Recording", "Exit");
 
     return 0;
 }
@@ -21,17 +24,22 @@ void header(string txt){
 }
 
 int choices(string txt1, string txt2){
-    int picked = 2;
+    int picked;
 
-    cout << endl << endl;
-    cout << setw(20) << "[1] " << txt1 << endl;
-    cout << setw(20) << "[2] " << txt2 << endl;
-    cout << endl << endl << endl;
-
-    cout << "Enter your choice: ";
+    do {
+        cout << endl << endl;
+        cout << setw(20) << "[1] " << txt1 << endl;
+        cout << setw(20) << "[2] " << txt2 << endl;
+        cout << endl << endl << endl;
+        cout << "Enter your choice: ";
         cin >> picked;
+
+        if (picked != 1 && picked != 2) {
+            system("CLS");
+            header("GRADE ENCODING");
+        }
+
+    } while (picked != 1 && picked != 2);
 
     return picked;
 }
-
-
